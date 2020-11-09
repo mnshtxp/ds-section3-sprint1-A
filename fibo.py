4efaaf943eea2637415e1b5f643520b4c86b1737
mnshtxp <nuniwa@daum.net>
2020-11-09 19:23:22 +0900

    First commit

---
+++ fibo.py
@@ -0,0 +1,15 @@
+def add(num1, num2) :
+    return num1 + num2
+print(add(7,2))
+
+def min(num1, num2) :
+    return num1 - num2
+print(min(3, 8))
+
+def div(num1, num2) :
+    return num1 / num2
+print(div(234, 29))
+
+def mult(num1, num2) :
+    return num1 * num2
+print(mult(19, 44))
---
+++ print_numbers.py
@@ -0,0 +1,4 @@
+import print_hello
+print('numbers')
+
+print_hello.bye()
