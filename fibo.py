82524034ca20c124d5fd3d4926efe3eaef350281
mnshtxp <morethanparadisee@gmail.com>
2020-11-11 16:42:02 +0900

    add

--- fibo.py
+++ fibo.py
@@ -1,11 +1,15 @@
+# 더하기 함수 생성
 def add(num1, num2) :
     return num1 + num2
 
+# 빼기 함수 생성
 def min(num1, num2) :
     return num1 - num2
 
+# 나누기 함수 생성
 def div(num1, num2) :
     return num1 / num2
 
+# 곱하기 함수 생성
 def mult(num1, num2) :
     return num1 * num2
